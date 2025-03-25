f012a74d6adb544cdbe908cd7c11dae2fd54a2d3
RideSafeMNL <ridesafe.mnl2025@gmail.com>
2025-03-25 20:37:11 +0800

    second commit

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-d
 import Login from "./components/login";
 import SignUp from "./components/register";
 import Profile from "./components/profile";
-import UserDetailsPage from "./components/UserDetailsPage"; // New import for user details page
-import MotorDetailsPage from "./components/MotorDetailsPage"; // New import for motor details page
-import EmergencyDetailsPage from "./components/EmergencyDetailsPage"; // New import for emergency details page
+import UserDetailsPage from "./components/UserDetailsPage";
+import MotorDetailsPage from "./components/MotorDetailsPage";
+import EmergencyDetailsPage from "./components/EmergencyDetailsPage";
 import { auth } from "./components/firebase";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -25,11 +25,11 @@ function App() {
         <div className="auth-wrapper">
           <div className="auth-inner">
             <Routes>
-              {/* Redirect to Profile if user is logged in */}
-              <Route path="/" element={user ? <Navigate to="/profile" /> : <Navigate to="/login" />} />
+              {/* Set Register as the default entry point (No.1) */}
+              <Route path="/" element={<SignUp />} />
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<SignUp />} />
-              <Route path="/profile" element={<Profile />} />
+              <Route path="/profile" element={user ? <Profile /> : <Navigate to="/login" />} />
               {/* Additional routes for details pages */}
               <Route path="/userDetails" element={<UserDetailsPage />} />
               <Route path="/motorDetails" element={<MotorDetailsPage />} />
